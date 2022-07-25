8d34f3597169590017c8cca6ba365d8c0139f9f2
sinapradip <101270811+sinapradip@users.noreply.github.com>
2022-07-25 16:24:27 +0545

    Add files via upload

--- public/client.js
+++ public/client.js
@@ -1,12 +1,16 @@
 const list = document.getElementsByClassName('toggle-button')[0];
 const navbar = document.getElementsByClassName('navbar-links')[0];
 const main = document.getElementById('main-link');
+const about = document.getElementById('about-link');
 const services = document.getElementById('services-link');
 const contact = document.getElementById('contact-link');
 list.addEventListener('click', () => {
     navbar.classList.toggle('active')
 })
 main.addEventListener('click', () => {
+    navbar.classList.toggle('active')
+})
+about.addEventListener('click', () => {
     navbar.classList.toggle('active')
 })
 services.addEventListener('click', () => {
